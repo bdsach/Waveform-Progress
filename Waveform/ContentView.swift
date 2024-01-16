36507085b2adb4decea222bd01c2db107f9168c7
Bandit Silachai <mbandit001@yahoo.co.th>
2024-01-16 18:50:05 +0700

    Initial Commit

---
+++ Waveform/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Waveform
+//
+//  Created by Bandit Silachai on 16/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Waveform/WaveformApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveformApp.swift
+//  Waveform
+//
+//  Created by Bandit Silachai on 16/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WaveformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
